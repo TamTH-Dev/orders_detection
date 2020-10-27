a706e3edd56c8f5dd15fec38a348614b81503833
TamTH-Dev <tamth.dev@gmail.com>
2020-10-28 00:04:12 +0700

    Implement redis queue

--- app/views.py
+++ app/views.py
@@ -1,29 +1,41 @@
 import cv2
 import numpy as np
+import time
 import pprint
 from app import app
 from flask import request, jsonify
 from .image_processing import process
+from redis import Redis
+from rq import Queue, Retry
 
 pp = pprint.PrettyPrinter(indent=4)
+
+q = Queue(connection=Redis())
+
+
+def process_img(bytes_str):
+    delay = 2
+    time.sleep(delay)
+
+    try:
+        # Get image from its bytes string
+        img = cv2.imdecode(np.fromstring(
+            bytes_str, np.uint8), cv2.IMREAD_COLOR)
+
+        order_details_obj = process(img)
+        pp.pprint(order_details_obj)
+        print('Completed to detect image!')
+
+    except Exception as error:
+        raise RuntimeError(error)
 
 
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
+    q_len = len(q)
     if request.method == 'POST':
         if request.files:
-            # Get image's bytes string
-            bytes_str = request.files['image'].read()
-
-            # Get image from its bytes string
-            img = cv2.imdecode(np.fromstring(
-                bytes_str, np.uint8), cv2.IMREAD_COLOR)
-
-            order_details_obj = process(img)
-
-            # Convert from dictionary object to json
-            # response = make_response(jsonify(order_details_obj), 200)
-            # pp.pprint(response.get_json())
-    message = 'Image is uploaded successfully' if order_details_obj else 'Failed to upload image!'
-
-    return jsonify({'message': message})
+            # Push image's bytes string into queue
+            job = q.enqueue(
+                process_img, request.files['image'].read(), retry=Retry(max=5))  # Retry 5 times until removed from queue
+    return jsonify({'message': 'Image is uploaded successfully!'})
